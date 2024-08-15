779c116f5be9bc96260fc3023e81f99c7f45d9ab
Siddharth Singhania <siddharthsinghania0101@gmail.com>
2024-08-16 01:30:18 +0530

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 
 import './App.css'
+import Chat from './components/Chat/Chat'
+import Detail from './components/Detail/Detail'
+import List from './components/List/List'
 
 function App() {
 
 
   return (
     <>
+    <div className='container'>
+      <List/>
+      <Chat/>
+      <Detail/>
+    </div>
     </>
   )
 }
---
+++ src/components/Detail/Detail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Detail.css'
+function Detail() {
+  return (
+    <div className='detail'>Detail</div>
+  )
+}
+
+export default Detail
